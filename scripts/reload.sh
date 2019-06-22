80089d4a4b6b76c628e9f844a0e40e42dd218968
armi3 <fernandagonzalez@ufm.edu>
2019-06-22 16:49:14 -0600

    Reload environment.

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,1 +1,7 @@
 #!/bin/zsh
+source $DOTFILES/environment/.alias
+source $DOTFILES/environment/.functions
+source $DOTFILES/environment/.exports
+source $DOTFILES/environment/.macOS
+
+echo "reloaded!"
